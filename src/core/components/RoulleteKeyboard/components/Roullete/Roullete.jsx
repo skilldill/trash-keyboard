0993bc1bda3b98ac53777d3ae4abb92c8988c5d2
Alexander Utbanov <utbanovag@it2g.ru>
2020-12-08 18:05:50 +0500

    add vibration

--- src/core/components/RoulleteKeyboard/components/Roullete/Roullete.jsx
+++ src/core/components/RoulleteKeyboard/components/Roullete/Roullete.jsx
@@ -1,4 +1,5 @@
 import React, { useMemo, useState, useEffect } from "react";
+import brdige from "@vkontakte/vk-bridge";
 
 import "./style.scss";
 import { useTouch } from "../../../../../shared/hooks";
@@ -31,6 +32,12 @@ export const Roullete = (props) => {
 
   const lengthLayoutLent = useMemo(() => (62 * (layout.length - 1)) - 84, [layout]);
 
+  const handleDragMove = () => {
+    if (stateTranslateY % 62 === 0) {
+      bridge.send("VKWebAppTapticSelectionChanged", {}); 
+    }
+  }
+
   const handleDragEnd = () => {
     if (stateTranslateY > -lengthLayoutLent && stateTranslateY < 84) {
       const diff = (stateTranslateY - 84) % 62;
@@ -38,7 +45,7 @@ export const Roullete = (props) => {
       if (oldY > stateTranslateY) {
         setOldY(stateTranslateY - (diff + 84));
         addTransitionAnimation();
-        return setStateTranslateY(stateTranslateY - (diff + 62)); 
+        return setStateTranslateY(stateTranslateY - (diff + 62));
       } 
 
       if (oldY < stateTranslateY) {
@@ -79,7 +86,7 @@ export const Roullete = (props) => {
       className="roullete" 
       style={dragStyle}
       onTouchStart={handleTouchStart()}
-      onTouchMove={handleTouchMove()}
+      onTouchMove={handleTouchMove(handleDragMove)}
       onTouchEnd={handleTouchEnd(handleDragEnd)}
     >
       {layout.map((keyObj, i) => (
